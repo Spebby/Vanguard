4fc8764bd228b5a9eebd07dc63ee9e9bfb3473ea
JM704-m <myx704ri@gmail.com>
2025-11-24 08:14:21 -0800

    Implementation of bullet-enemy collision logic
    When a bullet fired by the player hits a red square, the red square disappears.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,31 +2,41 @@ using System;
 using Gilzoide.UpdateManager;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour, IFixedUpdatable {
+public class Bullet : MonoBehaviour, IFixedUpdatable
+{
     float _speed;
     float _lifetime;
     int _damage;
     byte _colour;
-    
-    public void Init(float speed, float lifetime, int damage, byte colour = 0) {
+
+    public void Init(float speed, float lifetime, int damage, byte colour = 0)
+    {
         _speed = speed;
         _lifetime = lifetime;
         _damage = damage;
         _colour = colour;
     }
-    
-    void OnEnable()  => this.RegisterInManager();
+
+    void OnEnable() => this.RegisterInManager();
     void OnDisable() => this.UnregisterInManager();
 
-    public void ManagedFixedUpdate() {
+    public void ManagedFixedUpdate()
+    {
         transform.position += transform.up * (_speed * Time.fixedDeltaTime);
         _lifetime -= Time.fixedDeltaTime;
-        if (_lifetime <= 0) {
+        if (_lifetime <= 0)
+        {
             Destroy(gameObject);
         }
     }
 
-    public void OnCollisionEnter2D(Collision2D other) {
-        throw new NotImplementedException();
+    // New collision detection code
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy") || other.GetComponent<EnemyController>() != null)
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
